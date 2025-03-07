ad09d483ef202c817bb845bc1824c103575035c4
DaoBaoKhaa <khadbse183221@fpt.edu.vn>
2025-03-07 11:39:25 +0700

    SkintypeServiceAPI

---
+++ Backend/SkincareBookingService/SkincareBookingService.BLL/DTOs/SkintypeServiceDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SkincareBookingService.BLL.DTOs
+{
+    public class SkintypeServiceDTO
+    {
+            public int SkintypeServiceId { get; set; }
+            public int? SkintypeId { get; set; }
+            public int? ServiceId { get; set; }
+        }
+}
---
+++ Backend/SkincareBookingService/SkincareBookingService.BLL/Interfaces/ISkintypeServiceService.cs
@@ -0,0 +1,11 @@
+﻿using SkincareBookingService.BLL.DTOs;
+
+namespace SkincareBookingService.BLL.Interfaces
+{
+    public interface ISkintypeServiceService
+    {
+        Task<List<SkintypeServiceDTO>> GetSkintypeServicesAsync();
+
+        Task<SkintypeServiceDTO> GetSkintypeServiceByIdAsync(int id);
+    }
+}
