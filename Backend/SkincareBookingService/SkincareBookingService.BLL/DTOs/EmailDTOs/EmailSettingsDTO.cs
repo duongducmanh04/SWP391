2512823c3e9c464ef9f7ddad84df26f307b7875c
DaoBaoKhaa <khadbse183221@fpt.edu.vn>
2025-03-15 01:06:24 +0700

    EmailOTPAPI

---
+++ Backend/SkincareBookingService/SkincareBookingService.BLL/DTOs/EmailDTOs/EmailSettingsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SkincareBookingService.BLL.DTOs.EmailDTOs
+{
+    public class EmailSettingsDTO
+    {
+        public string Mail { get; set; }
+        public string DisplayName { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Backend/SkincareBookingService/SkincareBookingService.BLL/Interfaces/IAuthService.cs
+++ Backend/SkincareBookingService/SkincareBookingService.BLL/Interfaces/IAuthService.cs
@@ -7,5 +7,12 @@ namespace SkincareBookingService.BLL.Interfaces
         Task<Account> AuthenticateAsync(string accountName, string password);
 
         Task<Account> RegisterAsync(string accountName, string password);
+
+        Task SendEmailAsync(string to, string subject, string body);
+        Task<bool> ForgotPasswordAsync(string email);
+        Task<bool> VerifyOtpAsync(string email, string otp);
+
+        Task<bool> ResetPasswordAsync(string email, string otp, string newPassword);
+
     }
 }
