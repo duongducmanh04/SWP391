ce7ded433307733bb25364598f9e868b5183ed5d
DuongDucManh <manhddse173244@fpt.edu.vn>
2025-03-16 10:33:56 +0700

    Create get top rating services

--- Backend/SkincareBookingService/SkincareBookingService.BLL/DTOs/ServiceDTO.cs
+++ Backend/SkincareBookingService/SkincareBookingService.BLL/DTOs/ServiceDTO.cs
@@ -3,14 +3,15 @@
     public class ServiceDTO
     {
         public int ServiceId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public decimal? Price { get; set; }
-        public string Image { get; set; }
+        public string Image { get; set; } = string.Empty;
 
         public int? Duration { get; set; }
-
-        public string ProcedureDescription { get; set; }
+        
+        public double? AverageStars { get; set; }
+        public string ProcedureDescription { get; set; } = string.Empty;
         public DateTime? CreatedAt { get; set; }
     }
 
