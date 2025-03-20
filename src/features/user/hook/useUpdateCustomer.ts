b1fbd24cb5156bddc0d79cfc394fa56d6b96bd93
phamminhlocse184295 <locpmse184295@fpt.edu.vn>
2025-03-20 10:33:56 +0700

    new api

---
+++ src/features/user/hook/useUpdateCustomer.ts
@@ -0,0 +1,30 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { CustomerDto } from "../dto/customer.dto";
+
+interface UpdateCustomerParams {
+  accountId: number;
+  role: string;
+  updateData: Partial<CustomerDto>; // Chỉ cập nhật một phần dữ liệu
+}
+
+// Hàm gọi API cập nhật thông tin khách hàng
+const updateCustomer = async ({
+  accountId,
+  role,
+  updateData,
+}: UpdateCustomerParams): Promise<CustomerDto | null> => {
+  const response = await axios.put(
+    `https://localhost:7071/updateCustomer/${accountId}/${role}`,
+    updateData
+  );
+
+  return response.data || null;
+};
+
+// Hook sử dụng React Query để cập nhật dữ liệu
+export const useUpdateCustomer = () => {
+  return useMutation<CustomerDto | null, Error, UpdateCustomerParams>({
+    mutationFn: updateCustomer,
+  });
+};
--- src/pages/CustomerProfilePage.tsx
+++ src/pages/CustomerProfilePage.tsx
@@ -1,7 +1,17 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Layout, Menu, Card, Spin, Alert, Avatar, List, Button } from "antd";
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+  Layout,
+  Menu,
+  Card,
+  Spin,
+  Alert,
+  Avatar,
+  List,
+  Button,
+  Input,
+  message,
+} from "antd";
 import {
   UserOutlined,
   ClockCircleOutlined,
@@ -13,7 +23,7 @@ import { BookingDto } from "../features/booking/dto/booking.dto";
 import { PagePath } from "../enums/page-path.enum";
 import useAuthStore from "../features/authentication/hooks/useAuthStore";
 import dayjs from "dayjs";
-import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const { Sider, Content } = Layout;
 
@@ -22,6 +32,12 @@ const CustomerProfile = () => {
   const searchParams = new URLSearchParams(location.search);
   const initialTab = searchParams.get("tab") || "personal";
   const [activeTab, setActiveTab] = useState(initialTab);
+  const [isEditing, setIsEditing] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+  });
   const navigate = useNavigate();
   const { user } = useAuthStore();
 
@@ -30,24 +46,8 @@ const CustomerProfile = () => {
     isPending,
     error,
   } = useGetCustomerProfile(user?.accountId, user?.role);
-
   const profile = Array.isArray(profileData) ? profileData[0] : undefined;
   const customers = profile?.customer?.[0] ?? null;
-
-  console.log("Profile Data:", JSON.stringify(profileData, null, 2));
-
-  if (isPending) {
-    return (
-      <Spin
-        size="large"
-        className="flex justify-center items-center h-screen"
-      />
-    );
-  }
-
-  if (error) {
-    return <p className="text-red-500">Lỗi: {error.message}</p>;
-  }
 
   const {
     data: bookings,
@@ -56,11 +56,50 @@ const CustomerProfile = () => {
     error: bookingError,
   } = useBookingHistory();
 
-  const handleNavigateToBookingDetail = (bookingId: number) => {
+  const handleNavigateToBookingDetail = (bookingId: string | number) => {
     navigate(`${PagePath.CUSTOMER_BOOKING_DETAIL}?tab=schedule`, {
       state: { bookingId },
     });
   };
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+    setFormData({
+      name: customers?.name || "",
+      email: customers?.email || "",
+      phoneNumber: customers?.phoneNumber || "",
+    });
+  };
+
+  const handleCancelClick = () => setIsEditing(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleUpdateClick = async () => {
+    try {
+      await axios.put(
+        `https://localhost:7071/updateCustomer/${customers?.customerId}`,
+        formData
+      );
+      message.success("Cập nhật thông tin thành công!");
+      setIsEditing(false);
+    } catch (err) {
+      console.error(err); // In lỗi ra console (hoặc có thể xóa đi)
+      message.error("Cập nhật thông tin thất bại!");
+    }
+  };
+
+  if (isPending)
+    return (
+      <Spin
+        size="large"
+        className="flex justify-center items-center h-screen"
+      />
+    );
+  if (error) return <p className="text-red-500">Lỗi: {error.message}</p>;
 
   return (
     <Layout style={{ minHeight: "100vh", background: "#f5f1eb" }}>
@@ -95,94 +134,113 @@ const CustomerProfile = () => {
               {activeTab === "personal" && (
                 <div style={{ textAlign: "center" }}>
                   <Avatar size={100} src={customers?.image} />
-                  <h3 style={{ marginTop: 10 }}>{customers?.name}</h3>
-                  <p>
-                    <strong>Email:</strong> {customers?.email}
-                  </p>
-                  <p>
-                    <strong>Số điện thoại:</strong> {customers?.phoneNumber}
-                  </p>
-                  <p>
-                    <strong>Tên tài khoản:</strong> {profile?.accountName}
-                  </p>
+                  {isEditing ? (
+                    <div>
+                      <Input
+                        placeholder="Tên"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        style={{ marginBottom: 10 }}
+                      />
+                      <Input
+                        placeholder="Email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                        style={{ marginBottom: 10 }}
+                      />
+                      <Input
+                        placeholder="Số điện thoại"
+                        name="phoneNumber"
+                        value={formData.phoneNumber}
+                        onChange={handleInputChange}
+                        style={{ marginBottom: 10 }}
+                      />
+                      <Button type="primary" onClick={handleUpdateClick}>
+                        Cập nhật
+                      </Button>
+                      <Button
+                        style={{ marginLeft: 10 }}
+                        onClick={handleCancelClick}
+                      >
+                        Hủy
+                      </Button>
+                    </div>
+                  ) : (
+                    <>
+                      <h3 style={{ marginTop: 10 }}>{customers?.name}</h3>
+                      <p>
+                        <strong>Email:</strong> {customers?.email}
+                      </p>
+                      <p>
+                        <strong>Số điện thoại:</strong> {customers?.phoneNumber}
+                      </p>
+                      <p>
+                        <strong>Tên tài khoản:</strong> {profile?.accountName}
+                      </p>
+                      <Button type="primary" onClick={handleEditClick}>
+                        Sửa thông tin
+                      </Button>
+                    </>
+                  )}
                 </div>
               )}
-
-              {activeTab === "schedule" && (
-                <>
-                  {isBookingLoading ? (
-                    <Spin tip="Đang tải lịch sử đặt lịch..." />
-                  ) : isBookingError ? (
-                    <Alert
-                      message={
-                        bookingError?.message || "Lỗi tải lịch sử đặt lịch"
-                      }
-                      type="error"
-                    />
-                  ) : bookings && bookings.length > 0 ? (
-                    <List
-                      itemLayout="vertical"
-                      dataSource={bookings}
-                      pagination={{
-                        pageSize: 10, // Hiển thị 5 lịch mỗi trang
-                        showSizeChanger: true, // Cho phép chọn số lượng lịch trên mỗi trang
-                        pageSizeOptions: ["5", "10", "20"], // Các lựa chọn số lịch mỗi trang
-                      }}
-                      renderItem={(booking: BookingDto) => (
-                        <List.Item
-                          style={{
-                            cursor: "pointer",
-                            width: "100%",
-                            padding: "16px",
-                            border: "1px solid #ddd",
-                            borderRadius: "8px",
-                            marginBottom: "12px",
-                          }}
-                          onClick={() =>
-                            handleNavigateToBookingDetail(booking.bookingId)
+              {activeTab === "schedule" &&
+                (isBookingLoading ? (
+                  <Spin tip="Đang tải lịch sử đặt lịch..." />
+                ) : isBookingError ? (
+                  <Alert
+                    message={
+                      bookingError?.message || "Lỗi tải lịch sử đặt lịch"
+                    }
+                    type="error"
+                  />
+                ) : bookings && bookings.length > 0 ? (
+                  <List
+                    itemLayout="vertical"
+                    dataSource={bookings}
+                    pagination={{
+                      pageSize: 10,
+                      showSizeChanger: true,
+                      pageSizeOptions: ["5", "10", "20"],
+                    }}
+                    renderItem={(booking) => (
+                      <List.Item
+                        style={{
+                          cursor: "pointer",
+                          padding: "16px",
+                          border: "1px solid #ddd",
+                          borderRadius: "8px",
+                          marginBottom: "12px",
+                        }}
+                        onClick={() =>
+                          handleNavigateToBookingDetail(booking.bookingId)
+                        }
+                      >
+                        <List.Item.Meta
+                          title={<strong>{booking.serviceName}</strong>}
+                          description={
+                            <>
+                              <p>
+                                <strong>Ngày:</strong>{" "}
+                                {dayjs(booking.date).format("DD-MM-YYYY")}
+                              </p>
+                              <p>
+                                <strong>Trạng thái:</strong> {booking.status}
+                              </p>
+                              <p>
+                                <strong>Địa điểm:</strong> {booking.location}
+                              </p>
+                            </>
                           }
-                        >
-                          <List.Item.Meta
-                            title={<strong>{booking.serviceName}</strong>}
-                            description={
-                              <>
-                                <p>
-                                  <strong>Ngày:</strong>{" "}
-                                  {dayjs(booking.date).format("DD-MM-YYYY")}
-                                </p>
-                                <p>
-                                  <strong>Trạng thái:</strong> {booking.status}
-                                </p>
-                                <p>
-                                  <strong>Địa điểm:</strong> {booking.location}
-                                </p>
-                              </>
-                            }
-                          />
-                          <Button
-                            type="primary"
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              handleNavigateToBookingDetail(booking.bookingId);
-                            }}
-                          >
-                            Xem Chi Tiết
-                          </Button>
-                        </List.Item>
-                      )}
-                    />
-                  ) : (
-                    <Alert
-                      message="Không có lịch sử đặt lịch."
-                      type="warning"
-                    />
-                  )}
-                </>
-              )}
-
-              {activeTab === "password" && (
-                <p>Thay đổi mật khẩu của khách hàng</p>
-              )}
+                        />
+                      </List.Item>
+                    )}
+                  />
+                ) : (
+                  <Alert message="Không có lịch sử đặt lịch." type="warning" />
+                ))}
             </Card>
           </Content>
         </Layout>
